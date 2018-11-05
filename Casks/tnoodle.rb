df261183cec60a64aaa6e1cb6eeda6b99ba8f03d
Lucas Garron <code@garron.net>
2018-11-05 00:18:17 -0800

    Add TNoodle.

---
+++ Casks/tnoodle.rb
@@ -0,0 +1,10 @@
+cask 'tnoodle' do
+  version '0.13.5'
+  sha256 '37530dfc332ee4df2b455a3cfe4d94b63a433e6add578546ab64395d1ee0cb3a'
+
+  url "https://www.worldcubeassociation.org/regulations/scrambles/tnoodle/TNoodle-WCA-0.13.5.jar"
+  name 'TNoodle'
+  homepage 'https://www.worldcubeassociation.org/regulations/scrambles/'
+
+  app "TNoodle-WCA-0.13.5.jar"
+end
