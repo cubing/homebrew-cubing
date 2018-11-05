aa3778346b06e03d6c90cb8cfb7d6014c14142a3
Lucas Garron <code@garron.net>
2018-11-05 00:41:27 -0800

    Add KSolve+ (originally Kåre Krig, modified by Michale Gottlieb).

---
+++ Formula/ksolve+.rb
@@ -0,0 +1,17 @@
+class Ksolvex < Formula
+  desc "KSolve"
+  homepage "https://github.com/cubing/ksolve"
+  url "https://github.com/cubing/ksolve/archive/osx-parallel.zip"
+  version "1.3a"
+  sha256 "1ee0740d1074a5290d2cafa8b00ec93f0e4d7b211ba21bbf511e2fd5950c78ad"
+
+  def install
+    system "g++", "source/main.cpp", "-o", "source/ksolve+"
+    bin.install "source/ksolve+"
+  end
+
+  test do
+    # `test do` w
+    system "false"
+  end
+end
