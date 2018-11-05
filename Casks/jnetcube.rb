b8e72fc8d40e0e5619fc5927ebdbcf5e7f5536d2
Lucas Garron <code@garron.net>
2018-11-05 00:24:12 -0800

    Add Chris Hunt's JNetCube.

---
+++ Casks/jnetcube.rb
@@ -0,0 +1,10 @@
+cask 'jnetcube' do
+  version '1.0'
+  sha256 '0fbfeb8e082be47e9cbde06df8b6e239dd287f267a991dbbc5bd3169eca9180c'
+
+  url 'https://downloads.sourceforge.net/project/jnetcube/jnetcube/1.0/jnetcube.zip'
+  name 'JNetCube'
+  homepage 'https://sourceforge.net/projects/jnetcube/'
+
+  app 'JNetCube.jar'
+end
