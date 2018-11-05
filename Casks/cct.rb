603aead4f6ea2f59e1712e4b0692decc2451793e
Lucas Garron <code@garron.net>
2018-11-05 09:22:14 -0800

    FREEZE.indexed

---
+++ Casks/cct.rb
@@ -0,0 +1,10 @@
+cask 'cct' do
+  version '0.9.5'
+  sha256 ''
+
+  url 'https://cct.cubing.net/files/cct-0.9.5_20091127.zip'
+  name 'CalCubeTimer'
+  homepage 'https://cct.cubing.net/'
+
+  app ''
+end
