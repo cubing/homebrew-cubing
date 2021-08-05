1be0d635cfc5b4535b70e26312460b1a770782d8
Lucas Garron <code@garron.net>
2021-08-05 01:02:05 -0700

    Update `twsearch`.
    In particular, `devel` blocks are no longer supported:
    https://github.com/Homebrew/brew/pull/7688

--- Formula/twsearch.rb
+++ Formula/twsearch.rb
@@ -1,13 +1,12 @@
 class Twsearch < Formula
   desc "Twisty Puzzle Search Program."
-  homepage "https://github.com/rokicki/twsearch"
-  url "https://github.com/rokicki/twsearch/archive/e3ffccd085e13937595cb32de54308a55b94573e.zip"
-  version "e3ffccd"
-  sha256 "3bfe5584ed48f13b30a635e45502dfecbe308b78cd7b8afed4624459b5341809"
+  homepage "https://github.com/cubing/twsearch"
+  url "https://github.com/cubing/twsearch/archive/131f661c9b22b00ccae579caadbccfa035d16a0a.zip"
+  version "131f661"
+  sha256 "4a159eb4246be7171861121bbdac45aed1355164b14c4bc49881d2fce2e0e109"
 
-  devel do
-    url "https://github.com/rokicki/twsearch/archive/master.zip"
-    version "devel"
+  head do
+    url "https://github.com/cubing/twsearch/archive/main.zip"
   end
 
   def install
