78501c202e29f6ae82a3818641f55e15c22d7f82
Lucas Garron <code@garron.net>
2023-01-28 20:27:18 -0800

    [twsearch] Change to `HEAD`-only.

--- Formula/twsearch.rb
+++ Formula/twsearch.rb
@@ -1,17 +1,11 @@
 class Twsearch < Formula
   desc "Twisty Puzzle Search Program."
   homepage "https://github.com/cubing/twsearch"
-  url "https://github.com/cubing/twsearch/archive/131f661c9b22b00ccae579caadbccfa035d16a0a.zip"
-  version "131f661"
-  sha256 "4a159eb4246be7171861121bbdac45aed1355164b14c4bc49881d2fce2e0e109"
-
-  head do
-    url "https://github.com/cubing/twsearch/archive/main.zip"
-  end
+  head 'https://github.com/cubing/twsearch.git', :branch => 'main'
 
   def install
-    system "make"
-    bin.install "twsearch"
+    system "make build"
+    bin.install "./build/bin/twsearch"
   end
 
   test do
