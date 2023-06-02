9abc1862fc62477e29dca4295d693932b5ef5f5c
Lucas Garron <code@garron.net>
2023-06-01 22:06:23 -0700

    [tnoodle.rb] Update to TNoodle v1.1.2.

--- Casks/tnoodle.rb
+++ Casks/tnoodle.rb
@@ -1,10 +1,10 @@
 cask 'tnoodle' do
-  version '0.15.0'
-  sha256 '457893f2f4466d299e7da2d463dff81397f846c98ecbf8b4ec4b280efa041a48'
+  version '1.1.2.jar'
+  sha256 '1d6177a68a8eb1276f495d9f9ae31ad46876970b888b602d2ffdf485419acccc'
 
-  url "https://www.worldcubeassociation.org/regulations/scrambles/tnoodle/TNoodle-WCA-0.15.0.jar"
+  url "https://www.worldcubeassociation.org/regulations/scrambles/tnoodle/TNoodle-WCA-1.1.2.jar"
   name 'TNoodle'
   homepage 'https://www.worldcubeassociation.org/regulations/scrambles/'
 
-  app "TNoodle-WCA-0.15.0.jar"
+  app "TNoodle-WCA-1.1.2.jar"
 end
