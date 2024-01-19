c39328aa7cbc42fa0a7cae156d4849cae1812c20
Lucas Garron <code@garron.net>
2024-01-18 23:57:08 -0800

    [cube-explorer.rb] Update to v5.14

--- Formula/cube-explorer.rb
+++ Formula/cube-explorer.rb
@@ -1,11 +1,12 @@
 class CubeExplorer < Formula
   desc "Cube Explorer by Herbert Kociemba."
   homepage "http://kociemba.org/cube.htm"
-  url "http://kociemba.org/downloads/cube513.zip"
-  version "5.13"
-  sha256 "a75d36378951c4fdbf868e739962fbb6748c19cbcb2c0709c3cf9d4e66243d8a"
+  url "http://kociemba.org/downloads/cube514.zip"
+  version "5.14"
+  sha256 "ec622d6c67967beaf7cf4c72a9bba88e04161be347750fb839897e28a7d05019"
 
-  depends_on "wine"
+  # 😢 https://github.com/orgs/Homebrew/discussions/3163#discussioncomment-2526187
+  # depends_on "homebrew/cask/wine-stable"
 
   patch :DATA
 
@@ -13,10 +14,22 @@ class CubeExplorer < Formula
     prefix.install "AnimCube.jar"
     prefix.install "cube.ini"
     prefix.install "cube.chm"
-    prefix.install "cube513htm.exe"
+    prefix.install "cube514htm.exe"
     bin.install "cube-explorer-htm"
-    prefix.install "cube513qtm.exe"
+    prefix.install "cube514qtm.exe"
     bin.install "cube-explorer-qtm"
+  end
+
+  def caveats
+    <<~EOS
+
+      The `wine` Homebrew dependency cannot be installed automatically (because it is a cask).
+      Please install `wine` separately using:
+      
+          brew install homebrew/cask/wine-stable
+          cube-explorer-htm # This works now.
+
+    EOS
   end
 end
 
@@ -28,7 +41,7 @@ new file mode 100755
 @@ -0,0 +1,4 @@
 +#!/usr/bin/env bash
 +
-+wine HOMEBREW_PREFIX/Cellar/cube-explorer/5.13/cube513htm.exe
++wine HOMEBREW_PREFIX/Cellar/cube-explorer/5.14/cube514htm.exe
 +
 
 new file mode 100755
@@ -37,5 +50,5 @@ new file mode 100755
 @@ -0,0 +1,4 @@
 +#!/usr/bin/env bash
 +
-+wine HOMEBREW_PREFIX/Cellar/cube-explorer/5.13/cube513qtm.exe
++wine HOMEBREW_PREFIX/Cellar/cube-explorer/5.14/cube514qtm.exe
 +
