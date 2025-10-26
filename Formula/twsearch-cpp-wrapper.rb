64d69b99f08028f6a512844fe9bcf439d3ede6b4
Lucas Garron <code@garron.net>
2025-10-26 01:42:19 -0700

    [twsearch-cpp-wrapper] Install using `nightly`.

--- Formula/twsearch-cpp-wrapper.rb
+++ Formula/twsearch-cpp-wrapper.rb
@@ -4,9 +4,15 @@ class TwsearchCppWrapper < Formula
   head 'https://github.com/cubing/twsearch.git', :branch => 'main'
 
   depends_on "rust" => :build
+  depends_on "rustup" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args(path: "src/cpp")
+    ENV.prepend_path "PATH", Formula["rustup"].bin
+    system "rustup", "toolchain", "install", "nightly-2025-10-01"
+
+    # TODO: `twsearch-cpp-wrapper` disables the `simd` flag on `twsearch`, so using nightly should not be necessary.
+    # Also see: https://github.com/cubing/twsearch/issues/152
+    system "cargo", "+nightly-2025-10-01", "install", *std_cargo_args(path: "src/cpp")
 
     generate_completions_from_executable(bin/"twsearch-cpp-wrapper", "completions")
   end
